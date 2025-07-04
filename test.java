8efdd69dde95e417713e15c688ce3e57e7a84c1d
KuyaJ2015 <iamjames.valmeo@gmail.com>
2025-07-04 13:38:52 +0800

    finde the changes

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 System.out.println();
 System.out.println("change");
 System.out.println("change");
-System.out.println("change");
+System.out.println("fdasfa");
 System.out.println("change");
 System.out.println("change");
 System.out.println("change");
